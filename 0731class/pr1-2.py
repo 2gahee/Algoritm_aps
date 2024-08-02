e97fd7757d11207dfd603e3207342ae1e71de719
계ì2gahee <lgh7956@naver.com>
2024-08-02 17:32:28 +0900

    0802업데ì트

---
+++ 0731class/pr1-2.py
@@ -0,0 +1,20 @@
+N= int(input())
+arr=[list(map(int, input().split())) for _ in range(N)]
+
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+
+total = 0
+for i in range(N):
+    for j in range(N): #N*N 배열의 모든 원소에 대해
+        s = 0  # 문제에서 원소와 인접원소의 차의 절댓값의 합 저장
+        #i, j원소의 4방향 원소에 대해
+        for k in range(4):
+            ni= i+di[k]
+            nj= j+dj[k]
+            if 0<=ni<N and 0<=nj<N:        #실존하는 인접원소 ni nj가 구해진거
+                s += abs(arr[i][j]-arr[ni][nj])
+        total += s
+
+print(total)
